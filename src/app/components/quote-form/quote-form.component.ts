6b57f4d763466b6a415e146bc25ebf28ef431b3a
victoria makena <makenavictoria1@gmail.com>
2022-04-11 15:05:00 +0300

    add submission functionality

--- src/app/components/quote-form/quote-form.component.ts
+++ src/app/components/quote-form/quote-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Quote } from './../../quote';
+import { Component, OnInit, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-quote-form',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+  newQuote = new Quote(0, '', '', '', 0, 0);
+  @Output() addQuote = new EventEmitter<Quote>();
+  
+  submitQuote() {
+    this.addQuote.emit(this.newQuote)
+  }
 
   constructor() { }
 
