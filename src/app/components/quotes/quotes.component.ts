cfd0fcdeb17d07176e25569b2e0a53adeb88be93
victoria makena <makenavictoria1@gmail.com>
2022-04-11 13:19:47 +0300

    add submitted by field

--- src/app/components/quotes/quotes.component.ts
+++ src/app/components/quotes/quotes.component.ts
@@ -8,13 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote(1, 'Nathan John', 'You live to die, but it is what you die for that is worth living.', 0, 0),
-    new Quote(2, 'Bruce Lee', 'Be like water, my friend.', 0, 0),
-    new Quote(3, 'Maya Angelou', 'I am a Woman Phenomenally.Phenomenal Woman,that is me.', 0, 0),
-    new Quote(4, 'Gandhi', 'You must be the change you wish to see in the world.', 0, 0),
-    new Quote(5, 'Joshua J. Marine', 'Challenges are what make life interesting and overcoming them is what makes life meaningful.', 0, 0),
+    new Quote(1, 'Nathan John', 'You live to die, but it is what you die for that is worth living.','Nathan John', 3, 0),
+    new Quote(2, 'Bruce Lee', 'Be like water, my friend.','Mr Net', 2, 0),
+    new Quote(3, 'Maya Angelou', 'I am a Woman Phenomenally.Phenomenal Woman,that is me.','Tori', 0, 0),
+    new Quote(4, 'Gandhi', 'You must be the change you wish to see in the world.','Mr Net', 0, 0),
+    new Quote(5, 'Joshua J. Marine', 'Challenges are what make life interesting and overcoming them is what makes life meaningful.','Mr Net', 0, 0),
     
   ];
+
+  array: number[] = this.quotes.map(quote => quote.upvote);
+  highest = Math.max(...this.array);
+
 
   constructor() { }
 
