65420c96a94147eaafb9cba8f63e4f28da54378a
victoria makena <makenavictoria1@gmail.com>
2022-04-17 17:36:15 +0300

    Auto-generated commit [ci skip]

--- src/app/timing.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TimingPipe } from './timing.pipe';
-
-describe('TimingPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TimingPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
